ec01bb22d32ffff3a1f4a6dbebfec1792d8f8f8a
Abdullah Adeoba <Adeobaabdullah@gmail.com>
2024-01-11 11:36:47 +0100

    Work on doing

--- script.js
+++ script.js
@@ -26,17 +26,19 @@ function addFront(){
     displayCart()
     }
 }
-function addBack(){
-    if(inp.value === ""){
-        error.style.display = "block"
+function editAny(){
+    var userIndex = prompt("Enter the index you want to change")
+    var replacement = prompt("What do you want to replace it with?")
+    if(userIndex ==""||replacement=="" ){
+        alert("You think say you dey wise")
     }else{
-    error.style.display = "none"
-    cart.push(inp.value)
-    document.getElementById("inp").value = ""
-    show.innerHTML = cart
-    displayCart()
+        cart.splice(userIndex-1,1,replacement)
+         show.innerHTML = cart
+        document.getElementById("inp").value = ""
+        displayCart()
     }
 }
+
 
 function deleteFront(){
     cart.shift(show.innerHTML)
@@ -53,11 +55,40 @@ function deleteBack(){
     displayCart()
 }
 
+function deleteAny(){
+    var deleteIndex = prompt("enter the index you want to delete")
+    var userResponse = confirm("Are you sure you want to delete!!!")
+    if (deleteIndex !== "" && userResponse == true){
+      cart.splice(deleteIndex-1,1)
+      displayCart()
+    } else{
+      alert("God save you ")
+    }
+  }
+
+  function deleteAll() {
+    if (cart.length == 0) {
+      show.innerHTML =`<p style="text-align:center;font-weight:bold;">No Input to be deleted</p>`
+    }else{
+      var userResponse = confirm("Are you sure you want to delete!!!")
+      if (userResponse = true) {
+        cart.splice(0)
+        show.innerHTML = `All Cart has been deleted successfully`
+      }else {
+        alert("Ori Yo E")
+      }
+
+    } 
+  }
+
 function displayCart() {
     show.innerHTML = ""
     for(i = 0; i < cart.length; i++){
         show.innerHTML += `
-            <p>${i + 1}.${cart[i]}</p>
+            <tr>
+                <td>${i + 1}.<td>
+                <td>${cart[i]}<td>
+            <tr>
         `
     }
 }
